ac2f961ca2e86d4d1807240a99b4a496d73e1b27
AKULA SRI HARSHA SRI SAI HANUMAN <116995673+AHarshaNaidu@users.noreply.github.com>
2024-02-17 12:46:22 +0530

    Update AlgoTrad.py

--- AlgoTrad.py
+++ AlgoTrad.py
@@ -5,7 +5,8 @@ from tensorflow.keras.models import load_model  # Updated import statement
 import streamlit as st
 import matplotlib.pyplot as plt
 
-model_path = '/home/adminuser/venv/lib/python3.9/site-packages/AlgoTrad/Stock Predictions Model.keras'
+# Define the file path relative to the root directory of your Streamlit Cloud app
+model_path = 'Stock Predictions Model.keras'
 
 # Function to load the Keras model
 @st.cache(allow_output_mutation=True)
@@ -16,20 +17,20 @@ model = load_keras_model(model_path)
 
 st.header('Stock Market Predictor')
 
-stock =st.text_input('Enter Stock Symbol', 'GOOG')
+stock = st.text_input('Enter Stock Symbol', 'GOOG')
 start = '2012-01-01'
 end = '2022-12-31'
 
-data = yf.download(stock, start ,end)
+data = yf.download(stock, start, end)
 
 st.subheader('Stock Data')
 st.write(data)
 
 data_train = pd.DataFrame(data.Close[0: int(len(data)*0.80)])
-data_test = pd.DataFrame(data.Close[int(len(data)*0.80): len(data)])
+data_test = pd.DataFrame(data.Close[int(len(data)*0.80):])
 
 from sklearn.preprocessing import MinMaxScaler
-scaler = MinMaxScaler(feature_range=(0,1))
+scaler = MinMaxScaler(feature_range=(0, 1))
 
 pas_100_days = data_train.tail(100)
 data_test = pd.concat([pas_100_days, data_test], ignore_index=True)
@@ -37,7 +38,7 @@ data_test_scale = scaler.fit_transform(data_test)
 
 st.subheader('Price vs MA50')
 ma_50_days = data.Close.rolling(50).mean()
-fig1 = plt.figure(figsize=(8,6))
+fig1 = plt.figure(figsize=(8, 6))
 plt.plot(ma_50_days, 'r')
 plt.plot(data.Close, 'g')
 plt.show()
@@ -45,7 +46,7 @@ st.pyplot(fig1)
 
 st.subheader('Price vs MA50 vs MA100')
 ma_100_days = data.Close.rolling(100).mean()
-fig2 = plt.figure(figsize=(8,6))
+fig2 = plt.figure(figsize=(8, 6))
 plt.plot(ma_50_days, 'r')
 plt.plot(ma_100_days, 'b')
 plt.plot(data.Close, 'g')
@@ -54,7 +55,7 @@ st.pyplot(fig2)
 
 st.subheader('Price vs MA100 vs MA200')
 ma_200_days = data.Close.rolling(200).mean()
-fig3 = plt.figure(figsize=(8,6))
+fig3 = plt.figure(figsize=(8, 6))
 plt.plot(ma_100_days, 'r')
 plt.plot(ma_200_days, 'b')
 plt.plot(data.Close, 'g')
@@ -66,9 +67,9 @@ y = []
 
 for i in range(100, data_test_scale.shape[0]):
     x.append(data_test_scale[i-100:i])
-    y.append(data_test_scale[i,0])
+    y.append(data_test_scale[i, 0])
 
-x,y = np.array(x), np.array(y)
+x, y = np.array(x), np.array(y)
 
 predict = model.predict(x)
 
@@ -78,9 +79,9 @@ predict = predict * scale
 y = y * scale
 
 st.subheader('Original Price vs Predicted Price')
-fig4 = plt.figure(figsize=(8,6))
+fig4 = plt.figure(figsize=(8, 6))
 plt.plot(predict, 'r', label='Original Price')
-plt.plot(y, 'g', label = 'Predicted Price')
+plt.plot(y, 'g', label='Predicted Price')
 plt.xlabel('Time')
 plt.ylabel('Price')
 plt.show()
