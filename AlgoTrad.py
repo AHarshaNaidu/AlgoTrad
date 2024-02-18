d25a26121183af84fc81f7c446914a8cb9c28569
AKULA SRI HARSHA SRI SAI HANUMAN <116995673+AHarshaNaidu@users.noreply.github.com>
2024-02-18 18:04:03 +0530

    Update AlgoTrad.py

--- AlgoTrad.py
+++ AlgoTrad.py
@@ -29,7 +29,7 @@ end_date = st.sidebar.date_input("End date", datetime.date(2021, 1, 31))
 # Retrieving tickers data
 ticker_list_url = 'https://github.com/AHarshaNaidu/AlgoTrad/raw/main/Bombay.csv'
 ticker_list = pd.read_csv(ticker_list_url)
-tickerSymbol = st.sidebar.selectbox('Stock ticker', ticker_list['ticker'])  # Select ticker symbol
+tickerSymbol = st.sidebar.selectbox('Ticker', ticker_list['ticker'])  # Select ticker symbol
 tickerData = yf.Ticker(tickerSymbol)  # Get ticker data
 tickerDf = tickerData.history(period='1d', start=start_date, end=end_date)  # get the historical prices for this ticker
 
